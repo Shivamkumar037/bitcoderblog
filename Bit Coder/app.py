e7ad8f263f317b6327616735a591741112259007
Shivam Kumar <shivamkumar37je@gmail.com>
2025-04-07 07:26:36 +0530

    Update app.py

--- Bit Coder/app.py
+++ Bit Coder/app.py
@@ -1,15 +1,23 @@
-from flask import Flask,render_template
-app=Flask(__name__)
+from flask import Flask, render_template
+import os 
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
 @app.route("/posts")
 def post():
     return render_template("posts.html")
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
